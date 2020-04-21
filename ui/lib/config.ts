58685b04b5d39ba4bca2ff0fe4b90c1bad31146f
Umair Sarfraz <aquadestructor@icloud.com>
2020-04-21 16:58:32 +0500

    feat: update identity lib

--- ui/lib/config.ts
+++ ui/lib/config.ts
@@ -1,16 +1,26 @@
-const __DEV__ = process.env.NODE_ENV !== 'production'; // eslint-disable-line no-underscore-dangle
+/* eslint-disable no-underscore-dangle */
+const __DEV__ = process.env.NODE_ENV !== 'production';
+
+/** Determines if devnet should be used (instead of mainnet) */
+const __DEVNET__ = true;
 
 export const WEBSOCKETS_URL = __DEV__ ? 'http://localhost:8000' : 'https://selv01.iota.cafe:8888';
 
 export const API_URL = __DEV__ ? 'http://localhost:4000' : 'https://selv01.iota.cafe';
 
-export const IOTA_NODE_URL = 'https://nodes.thetangle.org:443';
+export const IOTA_NODE_URL = __DEVNET__ ? 'https://iri01.bare01.devnet.iota.cafe' : 'https://nodes.thetangle.org:443';
 
 export const RANDOM_USER_DATA_API_URL = 'https://randomuser.me/api/';
 
 export const KEY_ID = 'selv';
 
 export const SPLASH_SCREEN_TIMEOUT = 2000;
+
+export const MINIMUM_WEIGHT_MAGNITUDE = __DEVNET__ ? 9 : 14;
+
+export const DEPTH = 3;
+
+export const DEFAULT_TAG = 'COVID';
 
 export const DEFAULT_LANGUAGE = 'English';
 
