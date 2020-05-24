e259d9a4d10a6aef8a4d10718418ef17f5fbb3d1
Charlie Varley <charlie.varley@iota.org>
2020-05-24 15:23:29 +0100

    fix: avoid keyboard on Name page

--- ui/views/Name.svelte
+++ ui/views/Name.svelte
@@ -1,6 +1,7 @@
 <script>
-    import { Plugins } from '@capacitor/core';
+    import { Plugins, KeyboardInfo } from '@capacitor/core';
     import { onDestroy } from 'svelte';
+    import { flip } from 'svelte/animate';
 
     import Button from '~/components/Button';
     import TextField from '~/components/TextField';
@@ -13,17 +14,18 @@
     import { __WEB__ } from '~/lib/platform';
 
     const { Keyboard } = Plugins;
-
     let isCreatingCredentials = false;
     let firstName = '';
     let isKeyboardActive = false;
 
     let background;
+    let keyboardHeight;
 
     const isRunningOnMobile = !__WEB__;
 
     if (isRunningOnMobile) {
-        Keyboard.addListener('keyboardDidShow', () => {
+        Keyboard.addListener('keyboardWillShow', (info) => {
+            keyboardHeight = info.keyboardHeight;
             isKeyboardActive = true;
         });
 
@@ -151,6 +153,8 @@
         justify-content: space-between;
         overflow-y: auto;
         -webkit-overflow-scrolling: touch;
+        position: absolute;
+        padding: 6vh 5vw;
     }
 
     .content {
@@ -184,25 +188,32 @@
     }
 </style>
 
-<main bind:this="{background}" on:click="{handleOuterClick}" style="padding: {isKeyboardActive ? '0px 5vw 1vh' : '6vh 5vw'}">
+{#each [true] as item, index (item)}
+    <main
+        bind:this="{background}"
+        on:click="{handleOuterClick}"
+        style="top: {isKeyboardActive ? `-${keyboardHeight}px` : '0'}"
+        animate:flip="{{ duration: 350 }}"
+    >
 
-    <Header text="Set your first name" />
+        <Header text="Set your first name" />
 
-    <div class="content">
-        <img src="set-name.png" alt="" />
-    </div>
+        <div class="content">
+            <img src="set-name.png" alt="" />
+        </div>
 
-    <p class="info">Selv will generate you an identity using randomised personal information.</p>
+        <p class="info">Selv will generate you an identity using randomised personal information.</p>
 
-    <TextField disabled="{isCreatingCredentials}" bind:value="{firstName}" placeholder="First name" />
+        <TextField disabled="{isCreatingCredentials}" bind:value="{firstName}" placeholder="First name" />
 
-    <footer>
-        <Button
-            loading="{isCreatingCredentials}"
-            loadingText="{'Generating identity'}"
-            disabled="{firstName.length === 0}"
-            label="Save Name"
-            onClick="{save}"
-        />
-    </footer>
-</main>
+        <footer>
+            <Button
+                loading="{isCreatingCredentials}"
+                loadingText="{'Generating identity'}"
+                disabled="{firstName.length === 0}"
+                label="Save Name"
+                onClick="{save}"
+            />
+        </footer>
+    </main>
+{/each}
